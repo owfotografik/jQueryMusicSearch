798e2ba9c1799796484444beac4422ccad043ad4
OmasteWitkowski <owfotografik@gmail.com>
2017-11-15 17:44:26 -0700

    final project

--- jQueryMusicSearch.js
+++ jQueryMusicSearch.js
@@ -3,19 +3,19 @@ var MusicSearch = (function () {
     var songs = [];
     
     function MusicSearchConstructor() {
-    
+
     }
     
     MusicSearchConstructor.prototype.getAlbum = function (term, limit, callback) {
-       
-
         var apiUrl = `https://itunes.apple.com/search?term=${term}&limit=${limit}`;
-        
         var url = "http://bcw-getter.herokuapp.com/?url=" + encodeURIComponent(apiUrl);
 
         $.getJSON(url, function (data) {
+
  console.log(data);
+
              songs = data.results.map(function(song) {
+                 songs.includes("https://audio");
                  return {
                      title: song.trackName,
                      albumArt: song.artworkUrl60,
