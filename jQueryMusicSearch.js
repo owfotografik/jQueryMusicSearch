7c34c3cdade6ee4b60723a7258c683f372ca8dc3
OmasteWitkowski <owfotografik@gmail.com>
2017-11-15 19:36:02 -0700

    finish filter success

--- jQueryMusicSearch.js
+++ jQueryMusicSearch.js
@@ -1,38 +1,39 @@
 var MusicSearch = (function () {
-    
+
     var songs = [];
-    
+
     function MusicSearchConstructor() {
 
     }
-    
+
     MusicSearchConstructor.prototype.getAlbum = function (term, limit, callback) {
         var apiUrl = `https://itunes.apple.com/search?term=${term}&limit=${limit}`;
         var url = "http://bcw-getter.herokuapp.com/?url=" + encodeURIComponent(apiUrl);
 
         $.getJSON(url, function (data) {
 
- console.log(data);
+       // console.log(data);
+            var songs = data.results.filter(function (song) {
+                return song.kind === "song";
+            });
 
-             songs = data.results.map(function(song) {
-               if  (song.kind.includes("song")) {
-                 return {
-                     title: song.trackName,
-                     albumArt: song.artworkUrl60,
-                     artist: song.artistName,
-                     collection: song.collectionName,
-                     price: song.collectionPrice,
-                     preview: song.previewUrl
-                 }
+            songs = songs.map(function (song) {
+                return {
+                    title: song.trackName,
+                    albumArt: song.artworkUrl60,
+                    artist: song.artistName,
+                    collection: song.collectionName,
+                    price: song.collectionPrice,
+                    preview: song.previewUrl
+
                 }
-             })
-             console.log("songs:", songs);
-             callback(songs);
-            
-         });
-       
-    }
+            })
+            console.log("songs:", songs);
+            callback(songs);
+        });
 
-    
+}
+
+
     return MusicSearchConstructor;
-    })();
+}) ();
